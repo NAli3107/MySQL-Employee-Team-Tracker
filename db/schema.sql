1dcf9bf88fc8fbb1f1b4a38b2e990345a012c24f
Nadira Ali <nadiraali188a@gmail.com>
2022-03-14 21:01:57 +0000

    roles table created in company database

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,16 @@ CREATE TABLE departments(
     id INT NOT NULL AUTO_INCREMENT,
     names VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
+
+CREATE TABLE roles(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (7,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
 
