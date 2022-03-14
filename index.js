429aa1cdebade249a966156f9019413eed6b8130
Nadira Ali <nadiraali188a@gmail.com>
2022-03-14 20:44:12 +0000

    required mysql, inquirer and console.table packages in index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+/* Importing and requiring mySQL */
+const mysql = require('mysql2');
+
+/* Importing and requiring inquirer */
+const inquirer = require('inquirer');
+
+/* Importing and requiring console.table to print MySQL rows to the console */
+const cTable = require('console.table');
