b829b2176ffddaa550150b52289b04af5abfccac
Nadira Ali <nadiraali188a@gmail.com>
2022-03-14 21:12:55 +0000

    employees table created with primary and foreign keys linked

--- db/schema.sql
+++ db/schema.sql
@@ -15,8 +15,25 @@ CREATE TABLE roles(
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (7,2) NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id),
+    REFERENCES departments(id),
     ON DELETE SET NULL
 );
+
+
+CREATE TABLE employees(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id),
+    REFERENCES roles(id),
+    ON DELETE SET NULL,
+    PRIMARY KEY (id),
+    manage_id INT NOT NULL,
+    FOREIGN KEY (manager_id),
+    REFERENCES employees(id),
+    ON DELETE SET NULL,
+)
 
