79160b7966108b6fca4eb2d887a864cee182404c
Nadira Ali <nadiraali188a@gmail.com>
2022-03-16 21:08:31 +0000

    functions to render inquirer responses

--- index.js
+++ index.js
@@ -21,27 +21,103 @@ db.connect((err) => {
   promptMenu();
 });
 
+/* Inquirer prompts to render questions.*/
 const promptMenu = () => {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "selectOptions",
-      message: "What would you like to do?",
-      choices: [
-        "View all the departments",
-        "Add a department",
-        "Delete a department",
-        "View all the roles",
-        "Add a role",
-        "Delete a role",
-        "View all the employee",
-        "Add an employee",
-        "Delete an employee",
-        "Update an employee role",
-        `Update an employee manager's name`,
-        "Show employee by department",
-        "Quit",
-      ],
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "selectOptions",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Update an employee manager",
+          "View employees by department",
+          "Delete a department",
+          "Delete a role",
+          "Delete an employee",
+          "Quit",
+        ],
+      },
+    ])
+    .then((answers) => {
+      const { choices } = answers;
+
+      if (choices === "View all departments") {
+        renderDepartments();
+      }
+
+      if (choices === "View all roles") {
+        showRoles();
+      }
+
+      if (choices === "View all employees") {
+        renderEmployees();
+      }
+
+      if (choices === "Add a department") {
+        addDepartment();
+      }
+
+      if (choices === "Add a role") {
+        addRole();
+      }
+
+      if (choices === "Add an employee") {
+        addEmployee();
+      }
+
+      if (choices === "Update an employee role") {
+        updateEmployee();
+      }
+
+      if (choices === "Update an employee manager") {
+        updateManager();
+      }
+
+      if (choices === "View employees by department") {
+        employeeByDepartment();
+      }
+
+      if (choices === "Delete a department") {
+        deleteDepartment();
+      }
+
+      if (choices === "Delete a role") {
+        deleteRole();
+      }
+
+      if (choices === "Delete an employee") {
+        deleteEmployee();
+      }
+
+      if (choices === "Quit") {
+        db.quit();
+      }
+    });
 };
+
+function renderDepartments()
+function showRoles()
+function renderEmployees()
+function addDepartment()
+function addRole()
+function addEmployee()
+function updateEmployee()
+function updateManager()
+function employeeByDepartment()
+function deleteDepartment()
+function deleteRole()
+function deleteEmployee()
+
+
+
+
+
+
