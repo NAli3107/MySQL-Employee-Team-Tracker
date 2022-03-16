58f00cc7259112be456779976ab497c99ff0e5dc
Nadira Ali <nadiraali188a@gmail.com>
2022-03-16 20:12:21 +0000

    connection to database added in index.js file

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const inquirer = require('inquirer');
 
 /* Importing and requiring console.table to print MySQL rows to the console */
 const cTable = require('console.table');
+
+/* Connect to database */
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'company_db'
+    },
+  );
